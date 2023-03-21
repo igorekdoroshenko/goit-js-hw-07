6bbc5a442599cfb7af350b083d04ae225db7be26
igorekdoroshenko <igorek.doroshenko@gmail.com>
2023-03-21 23:38:34 +0200

    Finish

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,62 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const imagesEl = document.querySelector(".gallery");
+let instance;
+
+const galleryEl = galleryItems.map((image) => `<li class="gallery__item">
+  <a class="gallery__link" href="${image.original}">
+    <img
+      class="gallery__image"
+      src="${image.preview}"
+      data-source="${image.original}"
+      alt="${image.description}"
+    />
+  </a>
+</li>`).join("");
+
+imagesEl.insertAdjacentHTML("beforeend", galleryEl);
+
+imagesEl.addEventListener('click', clickOpenModal)
+
+function clickOpenModal(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+  `);
+    instance.show();
+    
+}
+
+imagesEl.addEventListener('click', clickCloseModal)
+
+function clickCloseModal() {
+  if (instance) { 
+    instance.close();
+  }
+}
+
+// window.addEventListener("keydown", closeModalOnEscape)
+
+// // imagesEl.removeEventListener("keydown", closeModalOnEscape)
+
+// function closeModalOnEscape(event) {
+//     if (event.code === 'Escape'&& instance) {
+//         instance.close();
+//         instance = null;
+//         window.removeEventListener("keydown", closeModalOnEscape); 
+//     }
+      
+// }
+
+
+
+// НЕ МОГУ ПОНЯТЬ ПОЧЕМУ ПО Escape не срабатывает
+
+
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,20 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const imagesEl = document.querySelector(".gallery");
+let instance;
+
+const galleryEl = galleryItems.map((image) => `<li class="gallery__item">
+   <a class="gallery__link" href="${image.original}">
+      <img class="gallery__image" src="${image.preview}" alt="${image.description}" />
+   </a>
+</li>`).join("");
+
+imagesEl.insertAdjacentHTML("beforeend", galleryEl);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
